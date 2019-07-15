0fffe9ccd3309d13e47dfa8ec2b1bc2299177282
aman <amanget4@gmail.com>
2019-07-15 18:20:39 -0500

    Project Starter

---
+++ src/main/java/com/ea/ecommercereviewservicebackend/ECommerceReviewServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ea.ecommercereviewservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceReviewServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceReviewServiceBackendApplication.class, args);
+	}
+
+}
